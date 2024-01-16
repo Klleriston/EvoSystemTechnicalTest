0278de2b3a962bac852a99c93f0aff5e312cae55
Klleriston Andrade <108811295+Klleriston@users.noreply.github.com>
2024-01-16 14:50:19 -0300

    Update Program.cs

--- API-EVOSYSTEMS/API.EVOSYSTEMS/Program.cs
+++ API-EVOSYSTEMS/API.EVOSYSTEMS/Program.cs
@@ -3,11 +3,7 @@ using API.EVOSYSTEMS.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -18,8 +14,6 @@ builder.Services.AddScoped<EmployeeService>();
 
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
